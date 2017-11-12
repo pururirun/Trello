c540f333df99f04aa3efebbdcd43ad4622a76514
pururirun <j.mardonescava@gmail.com>
2017-11-12 18:34:03 -0300

    Trello V.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,40 @@
-function textList() {
-  var container = document.getElementById('container');
-  var input = document.getElementById('input_principal');
-  container.removeChild(input);
+var btn_principal = document.getElementById("btn_principal");
+var contenedor = document.getElementById("contenedor");
 
-    //crear input y darle atributos//
+btn_principal.addEventListener("click", function(){ // Función para que aparezca input
+	var input = document.createElement("input");
+  var contenedor_input = document.createElement("div");
+  contenedor.removeChild(btn_principal); //removiendo btn_principal
+  contenedor_input.classList.add("class", "div_input");
+	input.setAttribute("type", "text");
+	input.setAttribute("placeholder", "Add a list...");
+	input.classList.add("class", "input_principal");
+	input.value;
 
-  var input = document.createElement("input");
-    input.setAttribute("type","text");
-    input.setAttribute("placeholder", "Add a list...");
-    input.classList.add("class", "input_titulo");
+	var btn = document.createElement("button"); //Creando botón Guardar
+	btn.setAttribute("id", "guardar");
+	var text_btn = document.createTextNode("Save"); //Texto dentro del botón
 
+  //papis 1
+	contenedor.appendChild(input);
+	btn.appendChild(text_btn);
+	contenedor.appendChild(btn);
+  contenedor.appendChild(contenedor_input);
+  contenedor_input.appendChild(input);
+  contenedor_input.appendChild(btn);
 
-    //crear boton y dar atributos al boton//
-    var nuevo_boton=document.createElement("button");
-    var text_boton = document.createTextNode("Guardar");
-    nuevo_boton.setAttribute("type","button");
+	var btn_guardar = document.getElementById("guardar");
+	btn_guardar.addEventListener("click", function(){
+	var contenedor_usuario = document.createElement("div");
+  contenedor_input.classList.add("class", "div_input");
+	var titulo_usuario = document.createElement("h4");
+	var text_titulo_usuario = document.createTextNode(input.value);
 
-    nuevo_boton.classList.add("class", "boton_guardar");
+  //papis 2
+  titulo_usuario.appendChild(text_titulo_usuario);
+  contenedor_usuario.appendChild(titulo_usuario);
+  contenedor.appendChild(contenedor_usuario);
 
+  })
 
-    var div_padre = document.getElementById("tareas");
-    div_padre.appendChild(input);
-    nuevo_boton.appendChild(text_boton);
-    div_padre.appendChild(nuevo_boton);
-}
+})
